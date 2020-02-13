8e2a584e119cb1b55d90d8174980a303cac5be3e
mkay1375 <mkay1375@gmail.com>
2020-02-13 16:25:55 +0330

    Move sample config file

--- src/test/java/ir/mkay/AppTest.java
+++ src/test/java/ir/mkay/AppTest.java
@@ -4,6 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Unit test for simple App.
  */
@@ -13,8 +18,8 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() throws IOException, URISyntaxException {
+        Files.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("config.properties").toURI())).forEach(System.out::println);
         assertTrue( true );
     }
 }
