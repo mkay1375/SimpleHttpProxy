caecdadf726435f1828daf8b6950e4b607cb2159
mkay1375 <mkay1375@gmail.com>
2020-02-19 11:39:18 +0330

    Remove unused codes and comments

--- src/test/java/ir/mkay/AppTest.java
+++ src/test/java/ir/mkay/AppTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import static org.junit.Assert.assertTrue;
 
@@ -19,13 +17,6 @@ public class AppTest
      */
     @Test
     public void shouldAnswerWithTrue() throws IOException, URISyntaxException {
-        /* TODO Tests:
-             - timeout
-             - unknown host
-             - simple get/post/put...
-         */
-
-        Files.readAllLines(Paths.get(this.getClass().getClassLoader().getResource("config.properties").toURI())).forEach(System.out::println);
         assertTrue( true );
     }
 }
